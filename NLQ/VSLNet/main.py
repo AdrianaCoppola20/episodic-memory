65a68a0734244231dcb8d7a8c742908aebf0457a
AdrianaCoppola20 <s349230@studenti.polito.it>
2025-06-21 17:58:48 +0200

    Update main.py

--- NLQ/VSLNet/main.py
+++ NLQ/VSLNet/main.py
@@ -164,7 +164,8 @@ def main(configs, parser):
                 loc_loss = model.compute_loss(
                     start_logits, end_logits, s_labels, e_labels
                 )
-                total_loss = loc_loss + configs.highlight_lambda * highlight_loss
+                # total_loss = loc_loss + configs.highlight_lambda * highlight_loss
+                total_loss = loc_loss 
                 # compute and apply gradients
                 optimizer.zero_grad()
                 total_loss.backward()
@@ -176,8 +177,8 @@ def main(configs, parser):
                 if writer is not None and global_step % configs.tb_log_freq == 0:
                     writer.add_scalar("Loss/Total", total_loss.detach().cpu(), global_step)
                     writer.add_scalar("Loss/Loc", loc_loss.detach().cpu(), global_step)
-                    writer.add_scalar("Loss/Highlight", highlight_loss.detach().cpu(), global_step)
-                    writer.add_scalar("Loss/Highlight (*lambda)", (configs.highlight_lambda * highlight_loss.detach().cpu()), global_step)
+                    # writer.add_scalar("Loss/Highlight", highlight_loss.detach().cpu(), global_step)
+                    # writer.add_scalar("Loss/Highlight (*lambda)", (configs.highlight_lambda * highlight_loss.detach().cpu()), global_step)
                     writer.add_scalar("LR", optimizer.param_groups[0]["lr"], global_step)
 
                 # evaluate
