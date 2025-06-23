b05d29ac0bd0c01d75f73733117d48a7be286190
AdrianaCoppola20 <s349230@studenti.polito.it>
2025-06-23 16:07:57 +0200

    Update VSLNet.py

--- NLQ/VSLNet/model/VSLNet.py
+++ NLQ/VSLNet/model/VSLNet.py
@@ -70,7 +70,7 @@ class VSLNet(nn.Module):
         self.cq_attention = CQAttention(dim=configs.dim, drop_rate=configs.drop_rate)
         self.cq_concat = CQConcatenate(dim=configs.dim)
         # query-guided highlighting
-        self.highlight_layer = HighLightLayer(dim=configs.dim)
+        # self.highlight_layer = HighLightLayer(dim=configs.dim)
         # conditioned predictor
         self.predictor = ConditionedPredictor(
             dim=configs.dim,
@@ -130,7 +130,7 @@ class VSLNet(nn.Module):
         # h_score = self.highlight_layer(features, v_mask)
         # features = features * h_score.unsqueeze(2)
         start_logits, end_logits = self.predictor(features, mask=v_mask)
-        return h_score, start_logits, end_logits
+        return start_logits, end_logits
         
     def extract_index(self, start_logits, end_logits):
         return self.predictor.extract_index(
